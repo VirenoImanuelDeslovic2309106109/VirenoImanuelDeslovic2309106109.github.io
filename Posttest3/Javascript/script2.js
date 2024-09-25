8adaa3788b1642f1f598524a246491bff9ce55d2
Vireno <162291137+VirenoImanuelDeslovic2309106109@users.noreply.github.com>
2024-09-25 22:38:49 +0800

    Add files via upload

---
+++ Posttest3/Javascript/script2.js
@@ -0,0 +1,25 @@
+const hamburger = document.getElementById('hamburger');
+const navbarList = document.getElementById('navbar-list');
+hamburger.addEventListener('click', () => {
+    navbarList.style.display = navbarList.style.display === 'flex'? 'none' : 'flex';
+});
+
+window.onload = function() {
+    document.getElementById('popup').style.display = 'flex';
+}
+document.getElementById('close-popup').onclick = function() {
+    document.getElementById('popup').style.display = 'none';
+}
+const modeToggle = document.getElementById('mode-toggle');
+
+modeToggle.addEventListener('change', () => {
+    document.body.classList.toggle('dark-mode');
+    const navbarLinks = document.querySelectorAll('.navbar-item a');
+    navbarLinks.forEach(link => {
+        link.classList.toggle('dark-mode');
+    });
+    const elements = document.querySelectorAll('.navbar-section, .footer-copyright, .popup-content');
+    elements.forEach(element => {
+        element.classList.toggle('dark-mode');
+    });
+});
